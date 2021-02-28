12803dcf1eac46220849a9c4425c6b1f689acbf2
José Reis <jmreis15@hotmail.com>
2021-02-28 22:55:38 +0000

    temp code for generating simulation data

---
+++ temp_data_generation.R
@@ -0,0 +1,112 @@
+### packages
+packs <- c("tidyverse", "furrr", "detectseparation")
+for (pack in packs) {
+  
+  if (pack %in% installed.packages()){
+    
+    require(pack, character.only = TRUE)
+    
+  } else {
+    
+    install.packages(pack)
+    require(pack, character.only = TRUE)
+    
+  }
+}
+
+### function for generating logistic regression data
+logistic_sim <- function(n = 100, beta1 = 0.5, seed = 1234){
+  
+  set.seed(seed)
+  alpha <- 0 # centered data
+  df <- tibble(x = rnorm(n, 0, 1)) %>%
+    mutate(pi = plogis((alpha + beta * x)), ## P(Y = 1| X) = logit^-1 \pi(x) = \frac{exp(\alpha + x * \beta)}{1 + exp(\alpha + x * \beta)}
+           y = rbinom(n, 1, prob = pi))
+  
+  return(df)
+}
+## count the number of samples with complete separation out of 100 samples of size 25
+sep_fun <- function(df, quasi_tresh = 0.92, kosmidis = FALSE) {
+  
+  ## check if there is complete separation: Rule based
+  out <- df %>%
+    mutate(overlap_complete = if_else(x < 0 & y == 0 | x > 0 & y == 1,1,0),
+           overlap_quasi = if_else(x <= 0 & y == 0 | x >= 0 & y == 1,1,0)) %>%
+    mutate(n_complete = sum(overlap_complete),
+           n_quasi = sum(overlap_quasi)) %>%
+    mutate(complete_separation = n_complete == nrow(.),
+           tresh_quasi_separation = n_quasi > nrow(.) * quasi_tresh)
+  
+  ## check if there is separation using Kosmidis and Konis algorithm
+  if (kosmidis & out$tresh_quasi_separation[1] == TRUE) {
+    
+    fit <- glm(y ~ x, family = binomial(link = "logit"), method = "detect_separation", data = out)
+    out$kosmidis_sep <- coef(fit)["x"] %in% c(Inf, -Inf)
+    
+  } else {
+    
+    out$kosmidis_sep <- FALSE
+    
+  }
+  
+  return(out)  
+}
+
+generate_separation_data <- function(population_size = 10000, sample_size = 50, beta = 4, seed = NULL){
+  
+  ### simulate population data
+  if (length(seed) == 0) {
+    
+    random_seed <- sample(1:99999999, 1) 
+    
+  } else {
+    
+    random_seed <- seed
+    
+  }
+  ### simulate population level data with a high regression coefficient
+  population <- logistic_sim(n = population_size, beta = beta, seed = random_seed)
+  ### draw several samples and assess whether there is identification: (i) rules based; (ii) if (i) assess using kosmidis et al algorithm
+  set.seed(random_seed)
+  reps <- population_size/sample_size ## repeated sampling: n repetitions
+  ### paralellization
+  no_cores <- availableCores() - 1
+  plan(multicore, workers = no_cores)
+  ### prepare the samples data, check if there is separation, and clean up
+  samples <- replicate(reps, sample_n(population, size = sample_size), simplify = FALSE) %>% ## sampling
+    furrr::future_map2_dfr(., 1:reps, ~ sep_fun(df = .x, kosmidis = TRUE) %>% ## assess separation
+                             mutate(sample_id = .y)) %>%
+    mutate(true_beta = beta, ## add the value of the true beta parameter, sample size, as well as the sample seed used for obtaining the population and sample data 
+           population_size = population_size,
+           sample_size = sample_size,
+           random_seed = random_seed,
+           population_id = paste(random_seed, beta, sep = "_"), ## add beta to population id case there were overlapping seeds
+           sample_id = paste(sample_id, population_id, sep = "_"),
+           separation = if_else(complete_separation == TRUE, "1", "0"),
+           quasi_separation = if_else(complete_separation == FALSE & kosmidis_sep == TRUE, "1", "0")) %>%
+    select(population_id, sample_id, population_size, sample_size, true_beta, y, x, pi, separation, quasi_separation, random_seed)
+  return(samples)
+}
+
+### generate the data
+## prepare the input grid: varying population parameters and sample sizes
+set.seed(1234)
+betas <- sample(seq(from = 0, to = 6, by = 0.01), 20)
+sample_size <- c(25, 50, 75, 100)
+simulation_grid <- expand_grid(betas, sample_size)
+### generate the populations and draw pop/sample_size samples
+sample_list <- map2(simulation_grid$betas, simulation_grid$sample_size, ~generate_separation_data(beta = .x, sample_size  = .y))
+## turn to one df
+all_samples <-  bind_rows(sample_list)
+## separate all data from samples with separation
+sep_samples <- all_samples %>%
+  filter(separation == "1" | quasi_separation == "1")
+### export
+if (!dir.exists("data")){
+  dir.create("data")
+}
+## all samples
+write_csv(all_samples, gzip("data/all_samples.csv.gz"))
+## samples with separation issues
+write_csv(all_samples, gzip("data/separation_samples.csv.gz"))
+
--- temp_make_sep_plot.R
+++ temp_make_sep_plot.R
@@ -1,12 +1,36 @@
-require(tidyverse)
+### packages
+packs <- c("tidyverse", "furrr")
+for (pack in packs) {
+  
+  if (pack %in% installed.packages()){
+    
+    require(pack, character.only = TRUE)
+    
+  } else {
+    
+    install.packages(pack)
+    require(pack, character.only = TRUE)
+    
+  }
+}
+## turn off scientific notation
+options(scipen=999)
 
-make_separation_plot <- function(n = 100, beta = 0.5, seed = 1234) {
+### function for generating logistic regression data
+logistic_sim <- function(n = 100, beta1 = 0.5, seed = 1234){
   
   set.seed(seed)
   alpha <- 0 # centered data
   df <- tibble(x = rnorm(n, 0, 1)) %>%
     mutate(pi = plogis((alpha + beta * x)), ## P(Y = 1| X) = logit^-1 \pi(x) = \frac{exp(\alpha + x * \beta)}{1 + exp(\alpha + x * \beta)}
            y = rbinom(n, 1, prob = pi))
+  
+  return(df)
+}
+### function for making the "separation plots"
+make_separation_plot <- function(n = 100, beta = 0.5, seed = 1234) {
+  
+  df <- logistic_sim(n = n, beta = beta, seed = seed)
   
   p <- df %>%
     ggplot(aes(x = x, y = y)) + 
@@ -18,12 +42,88 @@ make_separation_plot <- function(n = 100, beta = 0.5, seed = 1234) {
     stat_smooth(method = "glm", 
                 method.args = list(family = "binomial"),
                 se = FALSE) +
-    labs(subtitle = bquote(beta == .(beta)))
+    labs(subtitle = bquote(beta == .(beta) ~ "; " ~ n == .(n)),
+         y = bquote(pi[(x)] == beta~X))
   
   return(p) 
 }
 
-params <- c(2, 4, 12, 22)
-plots <- map(params, ~make_separation_plot(n = 50, beta = .x, seed = 1234))
+# small sample and separation
+params <- c(0.5, 1, 2, 4)
+plots <- map(params, ~make_separation_plot(n = 25, beta = .x, seed = 123))
+p1 <- cowplot::plot_grid(plotlist = plots)
+p1
+# large sample and separation
+params <- c(0.5, 1, 2, 4, 6)
+plots <- map(params, ~make_separation_plot(n = 500, beta = .x, seed = 123))
 cowplot::plot_grid(plotlist = plots)
+
+# separation as a consequence of the data
+params <- c(2, 6, 14, 22)
+plots <- map(params, ~make_separation_plot(n = 100, beta = .x, seed = 12))
+cowplot::plot_grid(plotlist = plots)
+
+
+
+### simulate a highly correlated population
+POP_N <- 100000
+SAMPLE_N <- 50
+REPS <- POP_N/SAMPLE_N
+BETA <- 5
+population <- logistic_sim(n = POP_N, beta = BETA, seed = 123)
+## plot
+population %>%
+  ggplot(aes(x = x, y = y)) + 
+  geom_point() +
+  annotate("rect", xmin = -Inf, xmax = 0, ymin = -Inf, ymax = .5, fill = "palegreen", alpha = 0.5) +
+  annotate("rect", xmin = 0, xmax = Inf, ymin = -Inf, ymax = .5, fill = "#F8766D", alpha = 0.5)  +
+  annotate("rect", xmin = -Inf, xmax = 0, ymin = Inf, ymax = .5, fill = "#F8766D", alpha = 0.5) +
+  annotate("rect", xmin = 0, xmax = Inf, ymin = Inf, ymax = .5, fill = "palegreen", alpha = 0.5) +
+  stat_smooth(method = "glm", 
+              method.args = list(family = "binomial"),
+              se = FALSE) +
+  labs(subtitle = bquote(beta == .(BETA) ~ "; " ~ n == .(POP_N)),
+       y = bquote(pi[(x)] == beta~X))
+## count the number of samples with complete separation out of 100 samples of size 25
+sep_fun <- function(df, quasi_tresh = 0.90, kosmidis = FALSE) {
+  
+  ## check if there is complete separation: Rule based
+  out <- df %>%
+    mutate(overlap_complete = if_else(x < 0 & y == 0 | x > 0 & y == 1,1,0),
+           overlap_quasi = if_else(x <= 0 & y == 0 | x >= 0 & y == 1,1,0)) %>%
+    mutate(n_complete = sum(overlap_complete),
+              n_quasi = sum(overlap_quasi)) %>%
+    mutate(complete_separation = n_complete == nrow(.),
+           quasi_complete_separation = n_quasi > nrow(.) * quasi_tresh)
+
+  ## check if there is separation using Kosmidis and Konis algorithm
+  if (kosmidis & out$quasi_complete_separation[1] == TRUE) {
+    
+    fit <- glm(y ~ x, family = binomial(link = "logit"), method = "detect_separation", data = out)
+    out$kosmidis_sep <- coef(fit)["x"] %in% c(Inf, -Inf)
+    
+  } else {
+    
+    out$kosmidis_sep <- NA
+    
+  }
+  
+  return(out)  
+}
+
+set.seed(1234)
+plan("multicore")
+### draw several samples and assess whether there is identification: (i) rules based; (ii) if (i) assess using kosmidis et al algorithm
+samples <- replicate(REPS, sample_n(population, size = SAMPLE_N), simplify = FALSE) %>%
+  furrr::future_map2_dfr(., 1:REPS, ~ sep_fun(df = .x, kosmidis = TRUE) %>%
+           mutate(sample_id = .y)) 
+
+### identify the samples containing separation
+sep_samples<- samples %>%
+  group_by(sample_id) %>%
+  summarise(rule_complete = mean(complete_separation),
+            rule_quasi = mean(quasi_complete_separation),
+            kosmidis_sep = mean(kosmidis_sep)) %>%
+  filter((rule_complete == 1 | rule_quasi == 1) & kosmidis_sep == 1)
+### double check with kosmidis
 
