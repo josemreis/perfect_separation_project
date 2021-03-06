26e4f2865c06f0a843053c3335e46f6d1a0e259b
josemreis <josembcsreis@gmail.com>
2021-03-06 19:04:58 +0000

    machine specific logger

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env Rscript
+
 cd "$(dirname "$0")"
 PC=$(hostname) # add user name to log for avoiding clashes between machines
-LOG_FILE="${PC}_simulation.log" 
+LOG_FILE="${PC}_simulation.log"
 Rscript simulation.R | tee ${LOG_FILE}
-
